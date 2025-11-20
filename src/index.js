c121aee2edff8be72018369d4a76dbcaf9a8cf78
PhuNetko <phu.phung@netko-solution.com>
2025-11-20 11:45:05 +0700

    hot fix to deploy

--- src/index.js
+++ src/index.js
@@ -21,14 +21,14 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-app.get("/", (req, res) => res.json({ message: "Backend is running phu oi" }));
+app.get("/", (req, res) => res.json({ message: "Backend is running" }));
 
 app.use("/cloudinary", cloudinaryRoutes);
 
 const PORT = process.env.PORT || 4000;
 
 const server = app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT} kkk`);
+  console.log(`Server running on port ${PORT}`);
 });
 
 // Handle server errors
